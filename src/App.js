0bfa9dcf895bb463924376aeb1ddb9e943013244
trevismcgill <trevismcgill25@gmail.com>
2020-12-01 11:22:18 -0600

    commit tree 1bceddd373096b95e8bfc2592ce6cd1cfa3b426a

--- src/App.js
+++ src/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    state = {
+        peopleInSpace: []
+    }
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+            .then(resp => resp.json())
+            .then(data => {
+                this.setState({
+                    peopleInSpace: data.people
+                })
+            })
+    }
+
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
